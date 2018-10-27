260ef26b21976029a43d23024f1b88f3e84fb654
Jason Hee <jasonheecs@gmail.com>
2018-10-27 17:36:09 +0800

    [ENHANCEMENT] Fix linting errors

--- src/progress-bar.js
+++ src/progress-bar.js
@@ -2,10 +2,10 @@ import { findObject } from 'semantic-dom-selectors';
 import buildAsserter from './assertions/index';
 
 export default function progress(label) {
-  const control = findObject(`[role='progressbar']`, label);
+  const control = findObject("[role='progressbar']", label);
   const asserter = buildAsserter(control, { label });
   asserter.valueNow = function (value) {
-    this.hasAttribute('aria-valuenow', '' + value);
+    this.hasAttribute('aria-valuenow', `${value}`);
   };
   return asserter;
 }
