4c2950ec8460baf738c385a7cfd72fedb7d5d3f6
Jason Hee <jasonheecs@gmail.com>
2018-10-27 16:47:07 +0800

    [REFACT] Refactor build pipeline
    * [BUGFIX] Add missing dependencies for rollup
    
    * [REFACT] Refactor build pipeline
      - Use Broccoli for build
      - Build semantic-dom-selectors dependency as well
    
    * [REFACT] Modify build pipeline to output combined and individual js assets

---
+++ Brocfile.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const babel = require('rollup-plugin-babel');
+const Rollup = require('broccoli-rollup');
+const Funnel = require('broccoli-funnel');
+const MergeTrees = require('broccoli-merge-trees');
+const concat = require('broccoli-concat');
+
+const semanticDomSelectors = new Funnel(path.dirname('./node_modules/semantic-dom-selectors/dist/semantic-dom-selectors.js'), {
+  include: ['semantic-dom-selectors.js']
+});
+
+const rollup = new Rollup('.', {
+  rollup: {
+    input: 'index.js',
+
+    plugins: [
+      babel({
+        exclude: 'node_modules/**'
+      })
+    ],
+
+    output: {
+      file: 'qunit-semantic-assertions.js',
+      format: 'amd',
+      amd: {
+        id: 'qunit-semantic-assertions'
+      },
+      // sourcemap: true,
+    },
+  }
+});
+
+const combinedJsAssets =  concat(new MergeTrees([rollup, semanticDomSelectors]), {
+  outputFile: '/combined.js'
+});
+
+module.exports = MergeTrees([combinedJsAssets, rollup]);
